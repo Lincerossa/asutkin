77539e38b92686e610ebbfccc72f931cb30a2f4e
marcello luatti <marcello.luatti@e-globe-solutions.com>
2020-05-26 23:05:24 +0200

    🚀 useCounter: a new hook

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,3 +1,4 @@
 export { default as useMouse } from './useMouse'
 export { default as useScrollDirection } from './useScrollDirection'
 export { default as usePrevious } from './usePrevious'
+export { default as useCounter } from './useCounter'
