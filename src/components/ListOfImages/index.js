ed7fe2eaf7ff834f0a8023d922017f4eb8489b82
marcello luatti <marcello.luatti@e-globe-solutions.com>
2020-06-01 19:41:46 +0200

    🚀 + 💅🏻 + 🔧 React.lazy on the Modal component because it will be imported only if it actually needs to be rendered (only in the desktop version)

--- src/components/ListOfImages/index.js
+++ src/components/ListOfImages/index.js
@@ -1,62 +1,48 @@
 import React, { useState} from 'react'
-import { Modal } from 'antd';
-import Slider from "react-slick";
-import Image from '../Image'
 
+import Image from '../Image'
 import List from '../List'
 
-import {
-  LeftOutlined,
-  RightOutlined,
-} from '@ant-design/icons';
-
 import * as S from './styles'
+import {isDesktop} from '../../utility'
 
-export default ({filtes, items}) => {
-  const [index, setIndex] = useState(null)
+const ModalSliderOfImages = React.lazy(() =>
+  import("../ModalSliderOfImages")
+)
+
+
+export default ({items}) => {
+  const [initialSlideIndex, setInitialSlideIndexIndex] = useState(null)
 
   function handleReset(){
-    setIndex(null)
+    setInitialSlideIndexIndex(null)
   }
 
 
   function handleImageClick(index){
-    if(typeof window !== "undefined" && window.innerWidth > 768) setIndex(index)
+
+   
+    if(isDesktop) setInitialSlideIndexIndex(index)
   }
 
-  const isModalVisible = index === 0 || index
+  const visible = initialSlideIndex === 0 || initialSlideIndex
+
   return (
   <>
-  
     <List columns={3}>
       {items.map((item, index) => <S.ListItem key={item.image.src} onClick={() => handleImageClick(index) }> <Image {...item} /></S.ListItem>)}
     </List>
 
-    <Modal
-      visible={isModalVisible}
-      mask
-      onCancel={handleReset}
-      onOk={handleReset}
-      footer={null}
-      title={null}
-      centered
-      height={typeof window !== "undefined" && window.innerHeight}
-      width={typeof window !== "undefined" && window.innerWidth }
-      bodyStyle={{
-        height: "100vh",
-      }}
-    >
-       { isModalVisible && <Slider {...{
-          dots: true,
-          arrows: true,
-          autoplay: false,
-          nextArrow: <RightOutlined />,
-          prevArrow: <LeftOutlined />,
-          initialSlide: index
-        }}>
-          {items.map((item) => <S.Slide><Image {...item} /></S.Slide>)}
-        </Slider>}
-    </Modal>
+    {isDesktop && (
+      <React.Suspense fallback={<div />}>
+        <ModalSliderOfImages
+          items={items}
+          visible={visible}
+          initialSlideIndex={initialSlideIndex}
+          handleReset={handleReset}
+        />
+      </React.Suspense>
+    )}
   </>
   )
 }
