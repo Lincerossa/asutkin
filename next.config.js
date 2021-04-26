80afe3a5854a9bb20131bc3d0037e6358917f0da
marcello luatti <marcello.luatti@e-globe-solutions.com>
2021-04-26 22:00:45 +0200

    ♻️ next.config.js micro refactor

--- next.config.js
+++ next.config.js
@@ -4,37 +4,23 @@ const data = require("./data.js")
 function createPage(route){
   const { slug, routes, componentIds} = route
   
-  const current = {
-    ...route,
-    slug,
-    currentRoute: route,
-    components: componentIds.map(id => data.components.find(component => component.id === id)).filter(e => e)
-  }
-  if(!routes) return current
-
   return [
-    current,
-    ...routes.map(innerRoute => createPage({...innerRoute, slug: `${route.slug}${innerRoute.slug}` }))
+    {
+      ...route,
+      slug,
+      currentRoute: route,
+      components: componentIds.map(id => data.components.find(component => component.id === id)).filter(e => e)
+    },
+    ...(routes ? routes.flatMap(innerRoute => createPage({...innerRoute, slug: `${route.slug}${innerRoute.slug}` })) : [])
   ]
 }
 
 module.exports = {
-  onDemandEntries: {
-    // period (in ms) where the server will keep pages in the buffer
-    maxInactiveAge: 25 * 1000,
-    // number of pages that should be kept simultaneously without being disposed
-    pagesBufferLength: 5,
-  },
-  exportPathMap: async function (
-    defaultPathMap,
-    { dev, dir, outDir, distDir, buildId }
-  ) {
+  exportPathMap: async function () {
 
-    const result = data.routes.map(createPage).flat(3).reduce((acc, val) => ({
+    return data.routes.flatMap(createPage).reduce((acc, val) => ({
       ...acc,
       [val.slug]: {page: '/main', query: val}
     }), {})
-
-    return result
   },
 }
